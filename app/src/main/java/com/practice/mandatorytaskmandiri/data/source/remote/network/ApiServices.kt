bf7cdb4b69ebb500ceaa6c3063df2cd9361dbe1d
mirvn <mirvan2599@gmail.com>
2023-01-11 23:43:53 +0700

    Fix ApiServices.kt - Fix getGenres method for adding Query

--- app/src/main/java/com/practice/mandatorytaskmandiri/data/source/remote/network/ApiServices.kt
+++ app/src/main/java/com/practice/mandatorytaskmandiri/data/source/remote/network/ApiServices.kt
@@ -7,19 +7,13 @@ import retrofit2.http.* // ktlint-disable no-wildcard-imports
 interface ApiServices {
 //    @POST("/login")
 //    suspend fun login(@Body requestAuth: AuthRequest): Response<AuthModel>
-//
-//    @POST("/register")
-//    suspend fun register(@Body requestAuth: AuthRequest): Response<AuthModel>
-//
+
 //    @POST("/transfer")
 //    suspend fun transfer(@Header("Authorization") auth: String,@Body requestTransferRequest: TransferRequest) :  Response<TransferModel>
 
-    @GET("/genre/movie/list")
-    suspend fun getGenres(@Query("api_key") apiKey: String): Response<GenreResponse>
-
-//    @GET("/payees")
-//    suspend fun payees(@Header("Authorization") auth: String) : Response<PayeesModel>
-//
-//    @GET("/transactions")
-//    suspend fun transactionsList(@Header("Authorization") auth: String): Response<TransactionListModel>
+    @GET("genre/movie/list")
+    suspend fun getGenres(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String
+    ): Response<GenreResponse>
 }
