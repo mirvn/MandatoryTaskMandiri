9b6dc3d1976c5d01f787d0742c38fd8cd902061b
mirvn <mirvan2599@gmail.com>
2023-01-12 18:26:45 +0700

    Add getMoviesByGenre Method

--- app/src/main/java/com/practice/mandatorytaskmandiri/data/source/remote/network/ApiServices.kt
+++ app/src/main/java/com/practice/mandatorytaskmandiri/data/source/remote/network/ApiServices.kt
@@ -1,6 +1,7 @@
 package com.practice.mandatorytaskmandiri.data.source.remote.network
 
 import com.practice.mandatorytaskmandiri.data.source.remote.response.GenreResponse
+import com.practice.mandatorytaskmandiri.data.source.remote.response.MoviesByGenreResponse
 import retrofit2.Response
 import retrofit2.http.* // ktlint-disable no-wildcard-imports
 
@@ -16,4 +17,16 @@ interface ApiServices {
         @Query("api_key") apiKey: String,
         @Query("language") language: String
     ): Response<GenreResponse>
+
+    @GET("discover/movie")
+    suspend fun getMoviesByGenre(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String,
+        @Query("sort_by") sort_by: String,
+        @Query("include_adult") include_adult: String,
+        @Query("include_video") include_video: String,
+        @Query("page") page: String,
+        @Query("with_genres") with_genres: String,
+        @Query("with_watch_monetization_types") with_watch_monetization_types: String,
+    ): Response<MoviesByGenreResponse>
 }
