15aa585f73a8d9568358ab3ea7bfc859b160e222
mirvn <mirvan2599@gmail.com>
2023-01-12 18:32:07 +0700

    Set navigation and add Bundle

--- app/src/main/java/com/practice/mandatorytaskmandiri/common/adapter/GenreListAdapter.kt
+++ app/src/main/java/com/practice/mandatorytaskmandiri/common/adapter/GenreListAdapter.kt
@@ -1,14 +1,19 @@
-package com.practice.mandatorytaskmandiri.ui.genres
+package com.practice.mandatorytaskmandiri.common.adapter
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.practice.mandatorytaskmandiri.R
 import com.practice.mandatorytaskmandiri.data.model.GenreModel
 import com.practice.mandatorytaskmandiri.databinding.GenreItemRvBinding
 
-class GenreListAdapter : ListAdapter<GenreModel, GenreListAdapter.GenresViewHolder>(diffCallBack) {
+class GenreListAdapter(private val navController: NavController) : ListAdapter<GenreModel, GenreListAdapter.GenresViewHolder>(
+    diffCallBack
+) {
     companion object {
         val diffCallBack = object : DiffUtil.ItemCallback<GenreModel>() {
             override fun areItemsTheSame(oldItem: GenreModel, newItem: GenreModel): Boolean {
@@ -24,7 +29,12 @@ class GenreListAdapter : ListAdapter<GenreModel, GenreListAdapter.GenresViewHold
     inner class GenresViewHolder(val view: GenreItemRvBinding) :
         RecyclerView.ViewHolder(view.root) {
         fun bind(model: GenreModel) {
+            val bundle = Bundle() // Bundle to pass genreId
+            bundle.putParcelable(itemView.context.getString(R.string.genre_model_key), model)
             view.tvGenreItem.text = model.name
+            view.imageButton.setOnClickListener {
+                navController.navigate(R.id.action_FirstFragment_to_SecondFragment, bundle)
+            }
         }
     }
 
