7ffd4e6e62faca55e48ca6b8abd535a6b0065af6
mirvn <mirvan2599@gmail.com>
2023-01-12 18:35:33 +0700

    Add model MovieByGenreModel

---
+++ app/src/main/java/com/practice/mandatorytaskmandiri/data/model/MovieByGenreModel.kt
@@ -0,0 +1,30 @@
+package com.practice.mandatorytaskmandiri.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class MovieByGenreModel(
+    val page: Int?,
+    val results: List<Result?>?,
+    val totalPages: Int?,
+    val totalResults: Int?
+) : Parcelable {
+    @Parcelize
+    data class Result(
+        val adult: Boolean?,
+        val backdropPath: String?,
+        val genreIds: List<Int?>?,
+        val id: Int?,
+        val originalLanguage: String?,
+        val originalTitle: String?,
+        val overview: String?,
+        val popularity: Double?,
+        val posterPath: String?,
+        val releaseDate: String?,
+        val title: String?,
+        val video: Boolean?,
+        val voteAverage: Double?,
+        val voteCount: Int?
+    ) : Parcelable
+}
