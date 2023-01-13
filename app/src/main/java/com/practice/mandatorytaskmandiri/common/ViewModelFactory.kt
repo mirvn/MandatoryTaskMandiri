039c857e432df3135e68a7845bee59942cafe56a
mirvn <mirvan2599@gmail.com>
2023-01-13 23:30:30 +0700

    adding detailMovies ViewModelProvider

--- app/src/main/java/com/practice/mandatorytaskmandiri/common/ViewModelFactory.kt
+++ app/src/main/java/com/practice/mandatorytaskmandiri/common/ViewModelFactory.kt
@@ -2,8 +2,10 @@ package com.practice.mandatorytaskmandiri.common
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.practice.mandatorytaskmandiri.data.repositories.DetailMovieRepoImplementation
 import com.practice.mandatorytaskmandiri.data.repositories.GenreRepoImplementation
 import com.practice.mandatorytaskmandiri.data.repositories.MoviesByGenreRepoImplementation
+import com.practice.mandatorytaskmandiri.ui.detailmovie.DetailMoviesViewModel
 import com.practice.mandatorytaskmandiri.ui.genres.GenreListFragmentViewModel
 import com.practice.mandatorytaskmandiri.ui.moviesbygenre.MoviesByGenreFragmentViewModel
 
@@ -21,6 +23,14 @@ class ViewModelFactory {
     ) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return MoviesByGenreFragmentViewModel(moviesByGenreRepoImplementation) as T
+        }
+    }
+
+    inner class ViewModelFactoryDetailMovies(
+        private val detailMovieRepoImplementation: DetailMovieRepoImplementation
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return DetailMoviesViewModel(detailMovieRepoImplementation) as T
         }
     }
 }
