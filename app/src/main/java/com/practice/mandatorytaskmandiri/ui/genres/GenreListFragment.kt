90b9b64ffcca18284286d811fd47627c158ecdf8
mirvn <mirvan2599@gmail.com>
2023-01-13 23:24:53 +0700

    set collapsingToolbar

--- app/src/main/java/com/practice/mandatorytaskmandiri/ui/genres/GenreListFragment.kt
+++ app/src/main/java/com/practice/mandatorytaskmandiri/ui/genres/GenreListFragment.kt
@@ -1,17 +1,19 @@
 package com.practice.mandatorytaskmandiri.ui.genres
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.practice.mandatorytaskmandiri.R
 import com.practice.mandatorytaskmandiri.common.ViewModelFactory
 import com.practice.mandatorytaskmandiri.common.adapter.GenreListAdapter
+import com.practice.mandatorytaskmandiri.common.helper.CollapsingToolbarHelper
 import com.practice.mandatorytaskmandiri.data.repositories.GenreRepoImplementation
 import com.practice.mandatorytaskmandiri.data.source.remote.network.ApiConfig
 import com.practice.mandatorytaskmandiri.data.source.remote.response.toListGenre
@@ -46,6 +48,18 @@ class GenreListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        CollapsingToolbarHelper().apply {
+            setImageToolbar(
+                activity?.findViewById(R.id.imgCollapsingToolbar),
+                null,
+                R.drawable.img
+            )
+        }.setTittleToolbar(
+            activity?.findViewById(R.id.collapsingToolbarLayout),
+            getString(R.string.movie_mania)
+        )
+        val imgCollapsedToolbar = activity?.findViewById<ImageView>(R.id.imgCollapsingToolbar)
+        imgCollapsedToolbar?.scaleType = ImageView.ScaleType.CENTER
         initRv()
         getGenres()
     }
@@ -63,7 +77,6 @@ class GenreListFragment : Fragment() {
                         // show rv
                         binding.progressBar5.visibility = View.INVISIBLE
                         val listGenre = result.data.toListGenre()
-                        Log.e(TAG, "getGenres: $listGenre ")
                         genreListAdapter.submitList(listGenre)
                     }
                     is ResponseUtil.Loading -> {
@@ -72,7 +85,6 @@ class GenreListFragment : Fragment() {
                     }
                     is ResponseUtil.Error -> {
                         binding.progressBar5.visibility = View.INVISIBLE
-                        Log.e(TAG, "getGenres: ${result.errorMessage}")
                         Toast.makeText(requireContext(), result.errorMessage, Toast.LENGTH_SHORT)
                             .show()
                     }
