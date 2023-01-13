3610eee4c39633ffc46df2019798b4720141bc5a
mirvn <mirvan2599@gmail.com>
2023-01-13 23:21:10 +0700

    add ApiServices method: getDetailMovie, getReviewMovie, getMovieTrailer

--- app/src/main/java/com/practice/mandatorytaskmandiri/data/source/remote/network/ApiServices.kt
+++ app/src/main/java/com/practice/mandatorytaskmandiri/data/source/remote/network/ApiServices.kt
@@ -1,17 +1,10 @@
 package com.practice.mandatorytaskmandiri.data.source.remote.network
 
-import com.practice.mandatorytaskmandiri.data.source.remote.response.GenreResponse
-import com.practice.mandatorytaskmandiri.data.source.remote.response.MoviesByGenreResponse
+import com.practice.mandatorytaskmandiri.data.source.remote.response.*
 import retrofit2.Response
 import retrofit2.http.* // ktlint-disable no-wildcard-imports
 
 interface ApiServices {
-//    @POST("/login")
-//    suspend fun login(@Body requestAuth: AuthRequest): Response<AuthModel>
-
-//    @POST("/transfer")
-//    suspend fun transfer(@Header("Authorization") auth: String,@Body requestTransferRequest: TransferRequest) :  Response<TransferModel>
-
     @GET("genre/movie/list")
     suspend fun getGenres(
         @Query("api_key") apiKey: String,
@@ -29,4 +22,25 @@ interface ApiServices {
         @Query("with_genres") with_genres: String,
         @Query("with_watch_monetization_types") with_watch_monetization_types: String
     ): Response<MoviesByGenreResponse>
+
+    @GET("movie/{movie_id}}")
+    suspend fun getDetailMovie(
+        @Path("movie_id") movie_id: Int,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String
+    ): Response<DetailMovieResponse>
+
+    @GET("movie/{movie_id}/reviews")
+    suspend fun getReviewMovie(
+        @Path("movie_id") movie_id: Int,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String,
+        @Query("page") page: Int
+    ): Response<MovieReviewResponse>
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun getMovieTrailer(
+        @Path("movie_id") movie_id: Int,
+        @Query("api_key") apiKey: String
+    ): Response<MovieTrailerResponse>
 }
